ac71817f809dfec96b22c6c3a224dfae224e7ed9
presjack <prof.prescot@gmail.com>
2017-11-16 03:16:37 +0000

    front end working, not connected to backend

---
+++ public/javascripts/app.js
@@ -0,0 +1,32 @@
+angular.module('questionApp', [])
+.controller('MainCtrl', [
+  '$scope', '$http',
+  function($scope,$http){
+    $scope.questions = [
+      {title:"Question1",comments:[{text:"this question is the best", upvotes:0}, {text:"the answer is yes", upvotes:0}]},
+      {title:"Question2",comments:[{text:"the answer is no",upvotes:0}, {text:"boo",upvotes:0}, {text:"go BYU!!!! 8)",upvotes:0}]}
+    ];
+
+    $scope.addQuestion = function() {
+      questions.push({title:$scope.questionFormContent,comments:[]})
+      $scope.questionFormContent='';
+    }
+
+    $scope.addComment = function(question) {
+      question.comments.push({title:question.commentFormContent,upvotes:0});
+      question.formContent='';
+    };
+
+    $scope.incrementUpvotes = function(comment) {
+      comment.upvotes += 1;
+    };
+
+/*    $scope.getAll = function() {
+      return $http.get('/comments').success(function(data){
+        angular.copy(data, $scope.comments);
+      });
+    };*/
+
+    //$scope.getAll();
+  }
+]);
